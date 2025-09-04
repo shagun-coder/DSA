914792380fcecc9dd86b992d7cda2622cfefce09
shagun-coder <shagunpadha108@gmail.com>
2025-09-04 22:44:22 +0530

    Added Spiral Matrix, Matrix Multiplication, and Matrix Wave programs

---
+++ MatrixMultiplication.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int m;
+    cout<< "rows of first matrix:";
+    cin >> m;
+    int n;
+    cout<<"columns of second matrix:";
+    cin >>n;
+    int p;
+    cout<< "rows of 2nd matrix:";
+    cin >> p;
+    int q;
+    cout<<"columns of 2nd matrix:";
+    cin >>q;
+
+    if(n==p){
+        int a[m][n];
+        
+        cout<<"Enter element of first matrix :";
+        for(int i = 0; i<m;i++){
+            for(int j = 0; j<n;j++){
+                cin >> a[i][j];   
+            }
+        }
+
+        int b[p][q];
+        cout<<"Enter element of 2nd matrix :";
+        for(int i = 0; i<p;i++){
+            for(int j = 0; j<q;j++){
+                cin >> b[i][j];  
+            }
+        }
+
+        // resultant matrix....
+        int res[m][q];
+        for(int i = 0;i<m;i++){
+            for(int j = 0; j<q;j++){
+                res[i][j] = 0;
+                // multiply
+                for(int k = 0;k<n;k++){  
+                    res[i][j] += a[i][k]*b[k][j];
+                }
+            }
+        }
+
+        // print res matrix...
+        cout <<"Resultant matrix = \n";
+        for(int i = 0;i<m;i++){
+            for(int j = 0; j<q;j++){
+                cout << res[i][j]<<" ";
+            }
+            cout<< endl;
+        }
+    }
+    else{
+        cout <<"matrixes can not be multilied!";
+    }
+}
---
+++ MatrixWave.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+int main(){
+    int m,n;
+    cout << "Enter the number of rows:";
+    cin>> m;
+    cout <<"Enter  the number of columns:";
+    cin >> n;
+    int arr[m][n];
+    for(int i = 0; i<m;i++){
+        for(int j = 0; j<n;j++){
+            cin >> arr[i][j];
+        }
+    }
+    // wave print
+    for(int i = 0; i<m;i++){
+        if(i%2==0){
+            for(int j = 0; j<n;j++){
+                cout <<arr[i][j]<<" ";
+            }
+        }
+        else{
+            for(int j = n-1;j>=0;j--){
+                cout << arr[i][j]<<" ";
+            }
+        }
+    }
+}
