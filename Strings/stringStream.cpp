30f4fbb3c41259343d6a2197c537e10fcb975422
Shagun <shagunpadha108@gmail.com>
2025-09-24 22:42:03 +0530

    Add files via upload

---
+++ Strings/stringStream.cpp
@@ -0,0 +1,19 @@
+/*Given a sentence ,split  every singlw word of the sentence and print it in new line*/
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<sstream>
+using namespace std;
+int main(){
+
+    string s ;
+    cout <<"Enter a string:";
+    getline(cin,s);
+    string temp;
+    stringstream ss(s);
+    
+    while(ss>>temp)
+    {
+        cout << temp<<endl;
+    }
+}
